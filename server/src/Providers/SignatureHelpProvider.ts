f4f422b343567dad355aceb3cdd2f9c4f2687026
Benjamin Auquite <halomastar@gmail.com>
2024-07-30 13:59:00 -0500

    can't recall what this change was

--- server/src/Providers/SignatureHelpProvider.ts
+++ server/src/Providers/SignatureHelpProvider.ts
@@ -1,4 +1,4 @@
-import { SignatureHelpParams } from "vscode-languageserver/node";
+import { SignatureHelp, SignatureInformation, SignatureHelpParams } from "vscode-languageserver/node";
 
 import type { ServerManager } from "../ServerManager";
 import type { FunctionComplexToken } from "../Tokenizer/types";
@@ -11,38 +11,45 @@ export default class SignatureHelpProvider extends Provider {
   constructor(server: ServerManager) {
     super(server);
 
-    this.server.connection.onSignatureHelp((params) => this.exceptionsWrapper(this.providerHandler(params)));
+    this.server.connection.onSignatureHelp((params) => this.exceptionsWrapper(() => this.providerHandler(params)));
   }
 
-  private providerHandler(params: SignatureHelpParams) {
-    return () => {
-      const {
-        textDocument: { uri },
-        position,
-      } = params;
-
-      const liveDocument = this.server.liveDocumentsManager.get(uri);
-      const document = this.server.documentsCollection.getFromUri(uri);
-      if (!liveDocument || !document) return;
-
-      const [lines, rawTokenizedContent] = this.server.tokenizer.tokenizeContentToRaw(liveDocument.getText());
-      const line = lines[position.line];
-      const tokensArray = rawTokenizedContent[position.line];
-
-      if (!tokensArray || !this.server.tokenizer.isInScope(tokensArray, position, LanguageScopes.functionCall)) return;
-
-      const rawContent = this.server.tokenizer.getLookBehindScopesRawContent(line, tokensArray, position, [LanguageScopes.functionCall, LanguageScopes.functionIdentifier]);
-      const activeParameter = this.server.tokenizer.getLookBehindScopeOccurences(tokensArray, position, LanguageScopes.separatorStatement, LanguageScopes.leftArgumentsRoundBracket);
-
-      const localScope = this.server.tokenizer.tokenizeContent(liveDocument.getText(), TokenizedScope.local, 0, position.line);
-      const functionComplexToken =
-        localScope.functionsComplexTokens.find((token) => token.identifier === rawContent) ||
-        document.getGlobalComplexTokens().find((token) => token.identifier === rawContent) ||
-        this.getStandardLibComplexTokens().find((token) => token.identifier === rawContent);
-
-      if (functionComplexToken) {
-        return SignatureHelpBuilder.buildFunctionItem(functionComplexToken as FunctionComplexToken, activeParameter);
+  private providerHandler(params: SignatureHelpParams): SignatureHelp | null {
+    const {
+      textDocument: { uri },
+      position,
+    } = params;
+  
+    const liveDocument = this.server.liveDocumentsManager.get(uri);
+    const document = this.server.documentsCollection.getFromUri(uri);
+    if (!liveDocument || !document) return null;
+  
+    const [lines, rawTokenizedContent] = this.server.tokenizer.tokenizeContentToRaw(liveDocument.getText());
+    const line = lines[position.line];
+    const tokensArray = rawTokenizedContent[position.line];
+  
+    if (!tokensArray || !this.server.tokenizer.isInScope(tokensArray, position, LanguageScopes.functionCall)) return null;
+  
+    const rawContent = this.server.tokenizer.getLookBehindScopesRawContent(line, tokensArray, position, [LanguageScopes.functionCall, LanguageScopes.functionIdentifier]);
+    const activeParameter = this.server.tokenizer.getLookBehindScopeOccurences(tokensArray, position, LanguageScopes.separatorStatement, LanguageScopes.leftArgumentsRoundBracket);
+  
+    const validActiveParameter = activeParameter !== null ? activeParameter : undefined;
+  
+    const localScope = this.server.tokenizer.tokenizeContent(liveDocument.getText(), TokenizedScope.local, 0, position.line);
+    const functionComplexToken =
+      localScope.functionsComplexTokens.find((token) => token.identifier === rawContent) ||
+      document.getGlobalComplexTokens().find((token) => token.identifier === rawContent) ||
+      this.getStandardLibComplexTokens().find((token) => token.identifier === rawContent);
+  
+    if (functionComplexToken) {
+      const helpItem = SignatureHelpBuilder.buildFunctionItem(functionComplexToken as FunctionComplexToken, validActiveParameter);
+  
+      if (helpItem) {
+        return helpItem as SignatureHelp;
       }
-    };
+    }
+  
+    return null;
   }
+  
 }
